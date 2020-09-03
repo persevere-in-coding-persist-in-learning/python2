7864f03627d76994c95ccafae1f8fb4904eec634
huijz <670478647@qq.com>
2020-09-03 11:54:58 +0800

    python 分支结构练习

---
+++ chapter05/pass_example.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+"""
+@desc: pass语句练习
+@auth: huijz
+@version: 1.0
+@date: 2020-09-03
+@email: huijz8117@gmail.com
+"""
+for letter in "Python":  # 第一个实例
+    if letter == 'h':
+        pass
+        print "pass letter", letter
+    print "当前的letter", letter
+print "GoodBye!"
